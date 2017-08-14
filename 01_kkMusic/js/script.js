4bbea4ceb112689523d5e69109843695373a38d4
李达康 <3075139617@qq.com>
2017-08-14 21:21:57 +0800

    Update script.js

--- 01_kkMusic/js/script.js
+++ 01_kkMusic/js/script.js
@@ -16,6 +16,117 @@ $(function(){
 	});
 
 	$("#newSong").slider();
+
+
+	//旋转木马
+	var $recommend = $('#recommend');
+	var $carousel_item = $recommend.find('.carousel-slider').find('.item');
+	var $carousel_prev = $recommend.find('.slider-prev');
+	var $carousel_next = $recommend.find('.slider-next');
+	var $carousel_btn = $recommend.find('.slider-btns').find('span');
+
+	var carouselArr = ['item-pic1','item-pic2','item-pic3','item-pic4','item-pic5','item-pic6'];
+	var b_stop = true;
+	var iNum = 0;
+
+	$carousel_btn.click(function(){
+		var cur_index = $(this).index();
+		var _num = cur_index - iNum;
+
+		if(iNum === cur_index){
+			return;
+		}else if(iNum < cur_index){
+			var newArr = carouselArr.splice(0,_num);
+			carouselArr = $.merge(carouselArr,newArr);
+
+			$carousel_item.each(function(i,elem){
+				$carousel_item.removeClass(carouselArr[i]);
+				$(elem).addClass(carouselArr[i]);
+			});
+		}else{
+			carouselArr.reverse();
+			var newArr = carouselArr.splice(0,-_num);
+			$.merge(carouselArr,newArr);
+			carouselArr.reverse();
+
+			$carousel_item.each(function(i,elem){
+				$carousel_item.removeClass(carouselArr[i]);
+				$(elem).addClass(carouselArr[i]);
+			});
+		}
+		setBtn(cur_index);
+		iNum = cur_index;
+	});
+
+	$carousel_prev.click(function(){
+		if(b_stop){
+			b_stop = false;
+			prevImg();
+		}
+		return false;
+	});
+	$carousel_next.click(function(){
+		if(b_stop){
+			b_stop = false;
+			nextImg();
+		}
+		return false;
+	});
+	$(document).on('click','#recommend .item-pic2',function(){
+		if(b_stop){
+			b_stop = false;
+			prevImg();
+		}
+		return false;
+	});
+	$(document).on('click','#recommend .item-pic4',function(){
+		if(b_stop){
+			b_stop = false;
+			nextImg();
+		}
+		return false;
+	});
+	function setBtn(index){
+		$carousel_btn.removeClass("cur");
+		$carousel_btn.eq(index).addClass("cur");
+	}
+	function prevImg(){
+		carouselArr.push(carouselArr.shift());
+
+		$carousel_item.each(function(i,elem){
+			$carousel_item.removeClass(carouselArr[i]);
+			$(elem).addClass(carouselArr[i]);
+
+			setTimeout(function(){
+				b_stop = true;
+			},300);
+		});
+
+		if(iNum === 0){
+			iNum = $carousel_item.length - 1;
+		}else{
+			iNum--;
+		}
+		setBtn(iNum);
+	}
+	function nextImg(){
+		carouselArr.unshift(carouselArr.pop());
+
+		$carousel_item.each(function(i,elem){
+			$carousel_item.removeClass(carouselArr[i]);
+			$(elem).addClass(carouselArr[i]);
+			setTimeout(function(){
+				b_stop = true;
+			},300);
+		});
+
+		if(iNum === $carousel_item.length - 1){
+			iNum = 0;
+		}else{
+			i=iNum++;
+		}
+		setBtn(iNum);
+	}
 });
 
 
