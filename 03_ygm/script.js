9fe8ac583ddbc325a93bf0afce2ab64f5175bdd2
李达康 <3075139617@qq.com>
2017-09-28 08:20:00 +0800

    Add files via upload

---
+++ 03_ygm/script.js
@@ -0,0 +1,23 @@
+/*window.onload=function(){
+
+};*/
+
+$(function(){
+$(window).scroll(function () {
+     // if($('body').width()<800)  return false;
+    if ($(this).scrollTop() > 100) {
+        $('#backtop').fadeIn();
+    } else {
+        $('#backtop').fadeOut();
+    }
+});
+
+// scroll body to 0px on click
+$('#backtop').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+});
+
+});
