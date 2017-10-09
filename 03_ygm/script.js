71f74dbe01f46262907aad56024fe6c5df62f7e4
李达康 <3075139617@qq.com>
2017-10-09 22:19:20 +0800

    Update script.js

--- 03_ygm/script.js
+++ 03_ygm/script.js
@@ -3,21 +3,26 @@
 };*/
 
 $(function(){
-$(window).scroll(function () {
-     // if($('body').width()<800)  return false;
-    if ($(this).scrollTop() > 100) {
-        $('#backtop').fadeIn();
-    } else {
-        $('#backtop').fadeOut();
-    }
-});
+    $(window).scroll(function () {
+         // if($('body').width()<800)  return false;
+        if ($(this).scrollTop() > 100) {
+            $('#backtop').fadeIn();
+        } else {
+            $('#backtop').fadeOut();
+        }
+    });
 
-// scroll body to 0px on click
-$('#backtop').click(function () {
-    $('body,html').animate({
-        scrollTop: 0
-    }, 500);
-    return false;
-});
+    // scroll body to 0px on click
+    $('#backtop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
 
+    $(window).resize(function(){
+        $(".header-top").css({
+            width:window.screen.width
+        });
+    });
 });
